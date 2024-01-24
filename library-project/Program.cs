0ad12f93968b86678153cd987d26f949c4dacdae
Alcides Neto <108681708+alcidesgoncalves@users.noreply.github.com>
2024-01-24 10:33:29 -0300

    initializing object

--- library-project/Program.cs
+++ library-project/Program.cs
@@ -4,8 +4,14 @@
     {
         public static void Main(string[] args)
         {
-            var book = new Book("", "", 315, true) {Title = "Atomic Habits", Author = "Frederico" };
-            Console.WriteLine(book);
+            var author = new Author("James Clear", "English", new DateTime(1986, 12, 16));
+            Console.WriteLine(author);
+
+            author.AddBook("Atomic Habits", 9791162540640, EStatus.Disponivel);
+            foreach (var book in author.Books)
+            {                
+                Console.WriteLine(book);
+            }
         }
     }
 }
