e2ff267df2231ecf042f20e85db7bf11db8b07e7
Alcides Neto <108681708+alcidesgoncalves@users.noreply.github.com>
2024-03-13 22:59:19 -0300

    feat/ListarAutores

--- library-project/Library.cs
+++ library-project/Library.cs
@@ -2,7 +2,33 @@ namespace Library
 {
     public class Library
     {
-        //Métodos: Adicionar Livro, Remover Livro, Listar Livros Disponíveis, 
-        //Listar Livros Emprestados, Listar Autores, etc.
+        //propriedades para armazenar listas de livros disponíveis, livros emprestados*, autores e usuários registrados.
+        public List<Author> AuthorList {get; set;}  
+        public List<Book> AvailableBooks {get; set;}
+        public List<Book> BorrowBooks {get; set;}
+        public List<User> UserList {get; set;} 
+
+        public Library() // CONSTRUTOR 
+        {
+            AvailableBooks = new List<Book>();
+            BorrowBooks = new List<Book>();
+            UserList = new List<User>();
+        }
+
+        public List<Author> ListarAutores()
+        {
+            List<Author> autores = new List<Author>();
+
+            foreach(var book in AvailableBooks)
+            {
+                if(!autores.Contains(book.Author))
+                {
+                    autores.Add(book.Author)
+                }
+            }
+
+            return autores; 
+        }
+        
     }
 }
