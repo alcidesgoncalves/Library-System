d22d1d44f80f1e94703b6653884d4e2c2ad1e83d
Alcides Neto <108681708+alcidesgoncalves@users.noreply.github.com>
2024-03-20 16:09:10 -0300

    User Class Comment out

--- library-project/User.cs
+++ library-project/User.cs
@@ -1,65 +1,65 @@
-namespace Library
-{
-    public class User
-    {
-        public string Name;
-        public int ID; 
-        public List<Book> BorrowedBooks;
-        public List<Book> BorrowHistory;
-        // Métodos: Realizar Empréstimo, Devolver Livro, Reservar Livro.
-        public User(string name, int id)
-        {
-            Name = name;
-            ID = id; 
-            BorrowedBooks = new List<Book>();
-            BorrowHistory = new List<Book>();
-        }
+// namespace Library
+// {
+//     public class User
+//     {
+//         public string Name;
+//         public int ID; 
+//         public List<Book> BorrowedBooks;
+//         public List<Book> BorrowHistory;
+//         // Métodos: Realizar Empréstimo, Devolver Livro, Reservar Livro.
+//         public User(string name, int id)
+//         {
+//             Name = name;
+//             ID = id; 
+//             BorrowedBooks = new List<Book>();
+//             BorrowHistory = new List<Book>();
+//         }
 
-        public override string ToString()
-        {
-            return $"Nome do usuário: {Name}, ID do usuário: {ID}";
-        }
+//         public override string ToString()
+//         {
+//             return $"Nome do usuário: {Name}, ID do usuário: {ID}";
+//         }
 
-        public EStatus RealizarEmprestimo(Book book)
-        {
-            if (book.Status == EStatus.Disponivel)
-            {
-                BorrowedBooks.Add(book);
-                book.BorrowBook();
-                return book.Status;
-            }
-            else
-            {
-                Console.WriteLine("O livro não está disponível para empréstimo.");
-                return EStatus.Disponivel;
-            }
-        }
+//         public EStatus RealizarEmprestimo(Book book)
+//         {
+//             if (book.Status == EStatus.Disponivel)
+//             {
+//                 BorrowedBooks.Add(book);
+//                 book.BorrowBook();
+//                 return book.Status;
+//             }
+//             else
+//             {
+//                 Console.WriteLine("O livro não está disponível para empréstimo.");
+//                 return EStatus.Disponivel;
+//             }
+//         }
 
-        public EStatus DevolverLivro(Book book)
-        {
-            bool LivroEncontrado = false; 
-            foreach (var BorrowedBook in BorrowedBooks)
-            {
-                if(BorrowedBook.Equals(book))
-                {
-                    BorrowedBooks.Remove(BorrowedBook);
-                    book.ReturnBook();
-                    BorrowHistory.Add(book);
-                    LivroEncontrado = true;
-                    break;
-                }
-            }
-            if (!LivroEncontrado)
-            {
-                Console.WriteLine("Você não possui este livro para devolver");
-            }
-            return book.Status;
-        }
+//         public EStatus DevolverLivro(Book book)
+//         {
+//             bool LivroEncontrado = false; 
+//             foreach (var BorrowedBook in BorrowedBooks)
+//             {
+//                 if(BorrowedBook.Equals(book))
+//                 {
+//                     BorrowedBooks.Remove(BorrowedBook);
+//                     book.ReturnBook();
+//                     BorrowHistory.Add(book);
+//                     LivroEncontrado = true;
+//                     break;
+//                 }
+//             }
+//             if (!LivroEncontrado)
+//             {
+//                 Console.WriteLine("Você não possui este livro para devolver");
+//             }
+//             return book.Status;
+//         }
 
-        public EStatus ReservarLivro(Book book)
-        {
-            book.ReserveBook();
-            return book.Status;
-        }
-    }
-}
+//         public EStatus ReservarLivro(Book book)
+//         {
+//             book.ReserveBook();
+//             return book.Status;
+//         }
+//     }
+// }
